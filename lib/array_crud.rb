26a8dc9e2735ba25879def65f631afb265c415a9
Reno Sorensen <renojames19@gmail.com>
2018-02-14 03:47:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Kelsier", "Vin", "Elend", "Sazed"]
-  array << "Breeze"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
